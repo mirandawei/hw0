eeb28451e31039a0e563052a86250d0d4e8974b3
Miranda Wei <weim@uchicago.edu>
2017-01-09 21:55:17 -0600

    yay

--- src/java/uchidb/HW0Runner.java
+++ src/java/uchidb/HW0Runner.java
@@ -1,50 +1,56 @@
 package uchidb;
 
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  * @author aelmore
  */
-public class HW0Runner {
+public class HW0Runner implements Containers<Integer, String> {
 
 
 	//TODO you likely will need to add member variable
-	public Map<Integer, String> localMap;
-	// instance?
+	private Map<String, Integer> localMap;
+
+	private static HW0Runner inst = new HW0Runner();
+	private HW0Runner(){};
 	
 	// This class is a factory for a singleton containers class.
 	// https://www.tutorialspoint.com/java/java_using_singleton.htm
 	public static Containers<Integer, String> getContainers() {
 		// TODO fix this function
-		return null;
+		return inst;
 	}
 	
 	// Create and return a set that stores the array of T objects
 	// You do not need to create your own set implementation and 
 	// can use an existing set class.
 	public Set<Integer> initSet(Integer[] tArray) {
-		Set<Integer> rv = new HashSet<Integer>(Arrays.asList(tArray));
-		return rv;
+		return new HashSet<Integer>(Arrays.asList(tArray));
 	}
 	
 	// Create and return a list that stores an the array of T objects
 	// You do not need to create your own list implementation and 
 	// can use an existing list class.
 	public List<Integer> initList(Integer[] tArray) {
-		List<Integer> rv = Arrays.asList(tArray);
-		return rv;
+		return new ArrayList<Integer>(Arrays.asList(tArray));
 	}
 	
 	// Create and return an empty map that will use type S as keys, and T as values
 	// You do not need to create your own map implementation and 
 	// can use an existing map class.
 	public Map<String,Integer> initEmptyMap() {
-		Map<String,Integer> rv = new HashMap<String,Integer>();
-		return rv;
+		return new HashMap<String,Integer>();
 	}
 	
 	// Store the map in a local field variable -- often called a setter 
 	public void storeMap(Map<String,Integer> mapToStoreInClass) {
-		Map<String, Integer> localMap = mapToStoreInClass;
+		localMap = mapToStoreInClass;
 	}
 	
 	// Add a key value to the stored/local map with a boolean indicating whether to overwrite existing value
@@ -55,10 +61,13 @@ public class HW0Runner {
 			if (overwriteExistingKey) {
 				localMap.put(key, value);
 				return true;
+			} else {
+				return false;
 			}
 		} else {
-			return false;
-		}	
+			localMap.put(key, value);
+			return true;
+		}
 	}
 	
 	// Return a value from the stored/local map based on the key
